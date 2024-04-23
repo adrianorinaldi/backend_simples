261955403f4402c9f9ec893955a74107535e241f
adrianorinaldi <adriano-rinaldi@hotmail.com>
2024-04-23 19:59:37 -0300

    teste5

--- src/main/java/com/example/demo/Controller/PessoaController.java
+++ src/main/java/com/example/demo/Controller/PessoaController.java
@@ -5,12 +5,14 @@ import com.example.demo.Service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin({"*"})
 @RestController
 @RequestMapping({"/"})
 public class PessoaController {
